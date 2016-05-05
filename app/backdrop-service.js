00b77cad692346c209f6ab5debff3a547baedfca
Damien Walsh <me@damow.net>
2016-05-05 16:46:02 +0100

    Update: Little bit hacky but just experimenting
    Issues around the backdrop height.

--- app/backdrop-service.js
+++ app/backdrop-service.js
@@ -2,7 +2,7 @@
 (function (app) {
     'use strict';
 
-    app.factory('uiTourBackdrop', ['TourConfig', '$document', '$uibPosition', function (TourConfig, $document, $uibPosition) {
+    app.factory('uiTourBackdrop', ['TourConfig', '$window', '$document', '$uibPosition', function (TourConfig, $window, $document, $uibPosition) {
 
         var service = {},
             $body = angular.element($document[0].body),
@@ -94,7 +94,7 @@
                 position: isFixedElement ? 'fixed' : 'absolute',
                 left: 0,
                 width: '100%',
-                height: (bodyPosition.top + bodyPosition.height - position.top - position.height) + 'px',
+                height: ($window.innerHeight - position.top - position.height) + 'px',
                 top: (position.top + position.height) + 'px'
             });
             viewWindow.left.css({
